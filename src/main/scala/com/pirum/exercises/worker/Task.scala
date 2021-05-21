1e46c9f68d1ac843cb806cc88a35a53da66bd0ff
Regis Kuckaertz <629976+regiskuckaertz@users.noreply.github.com>
2021-05-21 18:45:25 +0100

    Update Task.scala

--- src/main/scala/com/pirum/exercises/worker/Task.scala
+++ src/main/scala/com/pirum/exercises/worker/Task.scala
@@ -1,3 +1,6 @@
 package com.pirum.exercises.worker
 
-trait Task
+// A task that either succeeds after n seconds, fails after n seconds, or never terminates
+sealed trait Task {
+  def execute: Future[Unit]
+}
