90a0bee7fbbb2f25d7a2419e80a9ddcb578841a0
Norbert Gogiel <norbert.gogiel@fisglobal.com>
2021-10-27 00:13:40 +0100

    Add successful task execution to main

--- src/main/scala/com/pirum/exercises/worker/Main.scala
+++ src/main/scala/com/pirum/exercises/worker/Main.scala
@@ -1,10 +1,40 @@
 package com.pirum.exercises.worker
 
+import akka.actor.{ActorSystem, Props}
+
+import java.util.concurrent.{Executors, TimeUnit}
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.FiniteDuration
 
 object Main extends App with Program {
-  def program(tasks: List[Task], timeout: FiniteDuration, workers: Int): Unit =
-    ???
+  implicit val system: ActorSystem = ActorSystem("worker-pool")
 
-  println("Good luck 🤓")
+  val succeededTasksList: mutable.Buffer[String] = ListBuffer[String]()
+
+  def program(
+      tasks: List[Task],
+      timeout: FiniteDuration,
+      workers: Int
+  ): Unit = {
+    val executionContext: ExecutionContext =
+      ExecutionContext.fromExecutor(
+        Executors.newFixedThreadPool(workers)
+      )
+    val rootActor =
+      system.actorOf(
+        Props(
+          new ProgramActor(
+            succeededTasksList,
+            tasks,
+            timeout
+          )(system, executionContext)
+        )
+      )
+    rootActor ! ProcessTasks
+  }
+
+  val tasks = List(SuccessfulTask("Task1", FiniteDuration(3, TimeUnit.SECONDS)))
+  program(tasks, FiniteDuration(4, TimeUnit.SECONDS), tasks.size)
 }
