25ff8c7e96d8127d1a0369f6f8448c965b8f792c
Norbert Gogiel <norbert.gogiel@fisglobal.com>
2021-10-26 22:52:55 +0100

    Add successful task case

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ scalaVersion := "2.13.5"
 organization := "com.pirum"
 organizationName := "Pirum Systems"
 organizationHomepage := Some(url("https://www.pirum.com"))
+
+libraryDependencies := Seq(
+  "org.scalatest" %% "scalatest" % "3.1.4"
+)
---
+++ src/test/scala/com/pirum/exercises/worker/TaskSpec.scala
@@ -0,0 +1,22 @@
+package com.pirum.exercises.worker
+
+import org.scalatest.concurrent.Eventually
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+
+class TaskSpec extends AnyWordSpecLike with Matchers with Eventually {
+
+  "Task" should {
+    "succeed if SuccessfulTask is specified" in {
+      eventually(
+        SuccessfulTask(
+          "test-task",
+          FiniteDuration(1, TimeUnit.SECONDS)
+        ).execute.isCompleted
+      )
+    }
+  }
+}
